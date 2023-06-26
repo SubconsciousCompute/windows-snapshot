31b0ce9c4138ba642f71ad595bb40506e776c7ae
nidhih <nidhihemanth123@gmail.com>
2023-06-26 12:39:27 +0530

    made requested changes

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -9,10 +9,8 @@
 //! - [Cooling Device Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#cooling-device-classes)
 //! - [Input Device Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#input-device-classes)
 //! - [Mass Storage Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#mass-storage-classes)
-//! - [Motherboard, Controller, and Port Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#motherboard-controller-and-port-classes) (don't do)
 //! - [Networking Device Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#networking-device-classes)
 //! - [Power Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#power-classes)
-//! - [Printing Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#printing-classes) (don't do)
 //! - [Telephony Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#telephony-classes)
 //! - [Video and Monitor Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes#video-and-monitor-classes)
 
@@ -20,4 +18,5 @@ pub mod cooling_device;
 pub mod input_device;
 pub mod mass_storage;
 pub mod networking_device;
+
 pub mod telephony;
